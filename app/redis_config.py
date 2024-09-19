2fb5bfac85bc96ad83a036d4b3c70f663de7845e
Oluwatobi <tobiolajide01@gmail.com>
2024-09-18 21:51:01 -0400

    testing pr

--- app/redis_config.py
+++ app/redis_config.py
@@ -1,4 +1,4 @@
 import redis
 from rq import Queue
-r = redis.StrictRedis(host='backend-redis-1', port=6379, decode_responses=True)
+r = redis.StrictRedis(host='cosmosbackend-redis-1', port=6379, decode_responses=True)
 queue = Queue(connection=r)
